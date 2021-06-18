87b45bbdd2fef5076601eb55c8344dd23cb51762
harvestcamasu@gmail.com <harvestcamasu@gmail.com>
2021-06-19 07:49:11 +0800

    adjusted Connection method, not success

--- src/components/manekineko.vue
+++ src/components/manekineko.vue
@@ -39,7 +39,7 @@
         </div>
         </el-dialog>
 
-        <el-button type="primary" @click="endableBNB()"> Enable BNB</el-button>
+        <el-button type="primary" @click="connectMetamask()"> Enable BNB</el-button>
     </div>
 </template>
 
@@ -47,6 +47,7 @@
 
 const Web3 = require('web3');
 const web3 = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545');
+
 
 // Contract : Neko Collectibles - NCX
 import NekoCollectibleABI from '../web3/abi_nekocollectible';
@@ -93,130 +94,110 @@ const contractNeko = new web3.eth.Contract(NekoABI,contractAddressNeko);
         }
     },
     methods : {
-    async endableBNB(){
+        async connectBlockchain (){
+            //init the blockchain
+            await web3.request({ 
+                method: 'eth_requestAccounts',}).then((res) => {
+                    const accounts = res;
+                    this.account = Web3.utils.toChecksumAddress(accounts[0]);
+                    this.getNeko();
+                    this.getGeneralDetails();
+                    this.getNFTOwned();
+                    this.getNFTSupply();
+                    this.buyNFT();
+                }).catch((err) => {
+                    console.log(err, 'err');
+                });
+        },
+        buyNFT(){
+                this.payByCoin('999000100010','12');
+        },
+        payByCoin(_machine,_refNekoId){
+            console.log('payment PaymentGateway: ' ,PaymentGateway)
+            console.log('pay machine',_machine);
+            console.log('pay ref id',_refNekoId);
+            console.log('Wallet Acc',this.account);
 
-        if (window.ethereum) {
-        try {
-            const accounts = await web3.ethereum.request({ method: 'eth_requestAccounts' });
-
-
-            this.payByCoin('999000100010','12');
-        
-            console.log('new account :', accounts);
-        } catch (error) {
-            if (error.code === 4001) {
-            // User rejected request
-            }
-        }
-        }
-
-        //web3.ethereum.request({ method: 'eth_requestAccounts' });
-        //alert('yam : ', window.ethereum.isConnected());
-    },
-    async connectBlockchain (){
-        //init the blockchain
-        await window.ethereum.request({method: 'eth_requestAccounts'}).then((res) => {
-            const accounts = res;
-            this.account = Web3.utils.toChecksumAddress(accounts[0]);
-            this.getNeko();
-            this.getGeneralDetails();
-            this.getNFTOwned();
-            this.getNFTSupply();
-        });
-    },
-    buyNFT (){
-            this.payByCoin('999000100010','12');
-    },
-    payByCoin(_machine,_refNekoId){
-        console.log('payment PaymentGateway: ' ,PaymentGateway)
-        console.log('pay machine',_machine);
-        console.log('pay ref id',_refNekoId);
-        console.log('Wallet Acc',this.account);
-
-        contractPayment.methods.paymentByCoin(_machine,_refNekoId).send({
-            from: this.account,
-            value: 100000000000000000
+            contractPayment.methods.paymentByCoin(_machine,_refNekoId).send({
+                from: this.account,
+                value: 100000000000000000
+                }).then((res) => {
+                    console.log('Pay by coin :', res);
+                })
+        },
+        getNeko(){
+            contractNeko.methods.balanceOf(contractAddressCollectibles).call().then((res) => {
+                this.poolBalance = res;
+                console.log('async poolBalance :', res);
+            }).catch((err) => {
+                console.log(err, 'err');
+            });
+        },
+        getGeneralDetails(){
+            contractCollectibles.methods.balanceOf(this.account).call().then((res) => {
+                this.NFTBalance = res;
+                console.log('async You $neko Bal :', res);
+            }).catch((err) => {
+                console.log(err, 'err');
+            });
+        },
+        getNFTOwned (){        
+            contractCollectibles.methods.ownedNekos().call({
+                from: this.account,
             }).then((res) => {
-                console.log('Pay by coin :', res);
-            })
-    },
-    getNeko(){
-        contractNeko.methods.balanceOf(contractAddressCollectibles).call().then((res) => {
-            this.poolBalance = res;
-            console.log('async poolBalance :', res);
-        }).catch((err) => {
-            console.log(err, 'err');
-        });
-    },
-    getGeneralDetails(){
-        contractCollectibles.methods.balanceOf(this.account).call().then((res) => {
-            this.NFTBalance = res;
-            console.log('async You $neko Bal :', res);
-        }).catch((err) => {
-            console.log(err, 'err');
-        });
-    },
-    getNFTOwned (){        
-        contractCollectibles.methods.ownedNekos().call({
-            from: this.account,
-        }).then((res) => {
-            this.NFTOwned = res;
-            console.log('async Neko owned :', res);
-        }).catch((err) => {
-            console.log(err, 'err');
-        });
-
-    },
-    getNFTSupply (){        
-        contractCollectibles.methods.totalSupply().call({
-            from: this.account,
-        }).then((res) => {
-            this.NFTSupply = res;
-            console.log('async Neko owned :', res);
-        }).catch((err) => {
-            console.log(err, 'err');
-        });
-
-    },
-    getPaymentToken(){
-        
-            contractPayment.methods.totalTokens().call().then((res) => {
-                this.paymentTokenCount = res;
-                console.log('async Token count :', this.paymentTokenCount);
-
-
-                this.paymentTokens.push({
-                            tokenAddress: '0x0000000000000000000000000000000000001002',
-                            tokenSymbol: 'BNB',
-                            tokenQuantity: (1*10**17).toString(),
-                            tokenStatus: true
-                        });
-                        
-                        
-                for(let i=0; i< this.paymentTokenCount ;i++){ 
-
-                    contractPayment.methods.ERC20Tokens(i).call().then((res) => {
-                        this.paymentTokens.push({
-                            tokenAddress: res[0],
-                            tokenSymbol: res[1],
-                            tokenQuantity: res[2],
-                            tokenStatus: res[3],
-                        });
-                    }).catch((err) => {
-                        console.log(err, 'err');
-                    });
-                }
-                console.log ('Payment Tokens', this.paymentTokens);
-
+                this.NFTOwned = res;
+                console.log('async Neko owned :', res);
             }).catch((err) => {
                 console.log(err, 'err');
             });
 
-    },
-    payByToken(){
-  
-    },
+        },
+        getNFTSupply (){        
+            contractCollectibles.methods.totalSupply().call({
+                from: this.account,
+            }).then((res) => {
+                this.NFTSupply = res;
+                console.log('async Neko owned :', res);
+            }).catch((err) => {
+                console.log(err, 'err');
+            });
 
+        },
+        getPaymentToken(){
+            
+                contractPayment.methods.totalTokens().call().then((res) => {
+                    this.paymentTokenCount = res;
+                    console.log('async Token count :', this.paymentTokenCount);
+
+
+                    this.paymentTokens.push({
+                                tokenAddress: '0x0000000000000000000000000000000000001002',
+                                tokenSymbol: 'BNB',
+                                tokenQuantity: (1*10**17).toString(),
+                                tokenStatus: true
+                            });
+                            
+                            
+                    for(let i=0; i< this.paymentTokenCount ;i++){ 
+
+                        contractPayment.methods.ERC20Tokens(i).call().then((res) => {
+                            this.paymentTokens.push({
+                                tokenAddress: res[0],
+                                tokenSymbol: res[1],
+                                tokenQuantity: res[2],
+                                tokenStatus: res[3],
+                            });
+                        }).catch((err) => {
+                            console.log(err, 'err');
+                        });
+                    }
+                    console.log ('Payment Tokens', this.paymentTokens);
+
+                }).catch((err) => {
+                    console.log(err, 'err');
+                });
+
+        },
 
     }
 
